f7717838ef6ee2ea4f61e3c61a63ba60a51b75ce
Il'dar Valitov <valitov.i.r@gmail.com>
2023-11-05 00:50:52 +0100

    update

--- script.js
+++ script.js
@@ -2,35 +2,41 @@ var canvasOffset;
 var $painting;
 var $degree;
 var $current_degree;
-var $ms;
+var $rate;
 var degree = 0;
+var degree_step = 0.0174533;
+var rateMs = 10;
 var picWidth;
 var picHeight;
 var oX;
 var oY;
-var stepMs = 10;
 var isSpinning = false;
 var lineColor;
 var lineWidth;
 var ctx;
 var theCanvas;
+var letsdraw = false;
+var lineBroke = false;
+
+var cursorMoving = false;
+var lastCursorPosition = new Map();
 
 $(function () {
   $painting = $('#paint');
   $degree = $('#degree');
   $current_degree = $('#current_degree');
-  $ms = $('#ms');
+  $rate = $('#rate');
   $save_btn = $('#save_btn');
   var $select_color = $('#select_color')
   var $select_line_width = $('#select_line_width')
 
   // default
-  $ms.val(stepMs);
-  $degree.val(0.0174533);
+  $rate.val(rateMs);
+  $degree.val(degree_step);
 
-  var shorSide = window.innerWidth > window.innerHeight ? window.innerHeight / 1.5 :window.innerWidth;
+  var shortSide = window.innerWidth > window.innerHeight ? window.innerHeight / 1.5 : window.innerWidth / 1.5;
 
-  picWidth = shorSide * 0.95;
+  picWidth = shortSide * 0.95;
   picHeight = picWidth;
   oX = picWidth / 2;
   oY = picHeight / 2;
@@ -40,64 +46,62 @@ $(function () {
   theCanvas.width = picWidth;
   theCanvas.height = picHeight;
 
+  // grid_tools size
+
+  var tools_height = (window.innerHeight - picHeight) * 0.8;
+
+  $(".grid_tools").css("height", tools_height);
+
   canvasOffset = $('#paint').offset();
 
+  // disable selection
 
-  //color
+  $painting.disableSelection();
 
   createDefaultCirle();
 
   console.log("offset left: " + canvasOffset.left + ", offset top: " + canvasOffset.top);
 
-  var letsdraw = false;
+  $painting.on('mousemove touchmove', function (e) {
 
-    $painting.on('mousemove touchmove', function (e) {
     if (letsdraw === true) {
 
-      var x_base_Weighted;
-      var y_base_Weighted;
+      try {
 
-      if (e.type === 'mousemove') {
-        x_base_Weighted = e.pageX - canvasOffset.left;
-        y_base_Weighted = e.pageY - canvasOffset.top;
-      } else {
-        x_base_Weighted = e.originalEvent.changedTouches[0].pageX - canvasOffset.left;
-        y_base_Weighted = e.originalEvent.changedTouches[0].pageY - canvasOffset.top;
+        cursorMoving = true;
+
+        if (e.type === 'mousemove') {
+          lastCursorPosition.set('x', e.pageX);
+          lastCursorPosition.set('y', e.pageY);
+        } else {
+          lastCursorPosition.set('x', e.originalEvent.changedTouches[0].pageX);
+          lastCursorPosition.set('y', e.originalEvent.changedTouches[0].pageY);
+        }
+
+        draw();
+
+      } finally {
+        cursorMoving = false;
       }
 
-      var rotrateDegree = degree;
-
-      // convert to relative coordinates
-      var x_Relative = x_base_Weighted - oX;
-      var y_Relative = oY - y_base_Weighted;
-
-      // rotrate relative
-      var x_Relative_Rotrated = x_Relative * Math.cos(rotrateDegree) - y_Relative * Math.sin(rotrateDegree);
-      var y_Relative_Rotrated = x_Relative * Math.sin(rotrateDegree) + y_Relative * Math.cos(rotrateDegree);
-
-      // convert to original coordinates
-      var xResult = x_Relative_Rotrated + oX;
-      var yResult = oY - y_Relative_Rotrated;
-
-      ctx.lineTo(xResult, yResult);
-      ctx.stroke();
     }
   });
 
   $painting.on('mousedown touchstart', function (e) {
+    if (e.type === 'mousedown') {
+      lastCursorPosition.set('x', e.pageX);
+      lastCursorPosition.set('y', e.pageY);
+    } else {
+      lastCursorPosition.set('x', e.originalEvent.changedTouches[0].pageX);
+      lastCursorPosition.set('y', e.originalEvent.changedTouches[0].pageY);
+    }
     letsdraw = true;
-    ctx.strokeStyle = lineColor;
-    ctx.lineWidth = lineWidth;
-    ctx.beginPath();
+    beginLine();
   });
 
   $painting.on('mouseup touchend touchcancel', function (e) {
     letsdraw = false;
   });
-
-  $ms.on('input', function (e) {
-    stepMs = parseInt(e.target.value);
-  })
 
   $select_color.change(function (e) {
     lineColor = e.target.value;
@@ -115,18 +119,61 @@ $(function () {
     link.delete;
   });
 
-  $('#size_up').click(function() {
+  $('#size_up').click(() => {
     changeCanvasSize(100);
   });
 
-  $('#size_down').click(function() {
+  $('#size_down').click(() => {
     changeCanvasSize(-100);
   });
 
+  $('#drop_btn').click(() => {
+    createDefaultCirle();
+  })
+
+  $('#rotrate_btn').click(() => {
+    rotrateLoop();
+  })
+
+  $('#degree_increase_btn').click(() => {
+    $degree.val(degree_step = degree_step * 2);
+  })
+
+  $('#degree_decrease_btn').click(() => {
+    $degree.val(degree_step = degree_step / 2);
+  })
+
+  $degree.on('input', () => {
+    var value = parseFloat(this.value);
+    if (value != 0) {
+      degree_step = value;
+    }
+  })
+
+  $('#rate_increase_btn').click(() => {
+    $rate.val(rateMs = rateMs * 2);
+  })
+
+  $('#rate_decrease_btn').click(() => {
+    $rate.val(rateMs = rateMs / 2);
+  })
+
+  $rate.on('input', () => {
+    var value = parseFloat(this.value);
+    if (value != 0 && value > 0) {
+      rateMs = value;
+    }
+  })
 
 });
 
- function changeCanvasSize(px) {
+/*
+ *
+ * Functions
+ *
+ */
+
+function changeCanvasSize(px) {
   if (picWidth + px <= 0 || picHeight + px <= 0 ||
     picWidth + px >= window.innerWidth || picHeight + px >= window.innerHeight / 1.5) {
     return;
@@ -146,6 +193,8 @@ function createDefaultCirle() {
 }
 
 function createCirle(ctx, radius, color) {
+
+  ctx.clearRect(0, 0, picWidth, picHeight);
 
   ctx.fillStyle = color;
   ctx.beginPath();
@@ -167,7 +216,7 @@ function createCirle(ctx, radius, color) {
 
 function rotratePainting() {
 
-  degree = parseFloat($degree.val()) + degree;
+  degree = degree_step + degree;
   $painting.css('transform', "rotate(" + degree * (180 / Math.PI) + "deg)")
   $current_degree.html(degree);
 
@@ -184,10 +233,80 @@ async function rotrateLoop() {
 
   while (isSpinning) {
     rotratePainting();
-    await delay(parseInt($ms.val()));
+    if (letsdraw && !cursorMoving) {
+      draw();
+    }
+    await delay(parseInt(rateMs));
   }
 
 }
 
+async function draw() {
+
+  var x_base_Weighted = lastCursorPosition.get('x') - canvasOffset.left;
+  var y_base_Weighted = lastCursorPosition.get('y') - canvasOffset.top;
+
+  var rotrateDegree = degree;
+
+  // convert to relative coordinates
+  var x_Relative = x_base_Weighted - oX;
+  var y_Relative = oY - y_base_Weighted;
+
+  // rotrate relative
+  var x_Relative_Rotrated = x_Relative * Math.cos(rotrateDegree) - y_Relative * Math.sin(rotrateDegree);
+  var y_Relative_Rotrated = x_Relative * Math.sin(rotrateDegree) + y_Relative * Math.cos(rotrateDegree);
+
+  if (Math.abs(x_Relative_Rotrated) > oX || Math.abs(y_Relative_Rotrated) > oY || !inBounds(x_Relative_Rotrated, y_Relative_Rotrated)) {
+    breakLine();
+    return;
+  }
+
+  if (lineBroke) {
+    beginLine();
+  }
+
+  console.info("x: " + x_Relative_Rotrated + " y: " + y_Relative_Rotrated);
+
+  // convert to original coordinates
+  var xResult = x_Relative_Rotrated + oX;
+  var yResult = oY - y_Relative_Rotrated;
+
+  ctx.lineTo(xResult, yResult);
+  ctx.stroke();
+}
+
 const delay = async (ms) =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+
+$.fn.extend({
+  disableSelection: function () {
+    this.each(function () {
+      this.onselectstart = function () {
+        return false;
+      };
+      this.unselectable = "on";
+      $(this).css('-moz-user-select', 'none');
+      $(this).css('-webkit-user-select', 'none');
+    });
+    return this;
+  }
+});
+
+function inBounds(x, y) {
+  var result = Math.sqrt(Math.pow(picHeight / 2, 2) - Math.pow(x, 2));
+  return !isNaN(result) && Math.abs(y) < result;
+}
+
+function beginLine() {
+  lineBroke = false;
+  ctx.strokeStyle = lineColor;
+  ctx.lineWidth = lineWidth;
+  ctx.beginPath();
+}
+
+function breakLine() {
+  lineBroke = true;
+  ctx.stroke();
+}
+
